e57b664209c6f900fcbebd06e41945b8a19789fe
saku-1101 <sakupi1101@outlook.jp>
2023-07-08 13:17:22 +0900

    add: tailwind-styled-componentsとそれに伴うstorybookの設定

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,20 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        postCss: {
+          implementation: require.resolve('postcss'),
+        },
+      },
+    },
+  ],
   framework: {
     name: '@storybook/nextjs',
     options: {},
---
+++ src/components/atoms/Button/Button.stories.ts
@@ -0,0 +1,17 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from './Button';
+
+export type ButtonProps = {};
+
+export default {
+  title: 'Button',
+  component: Button,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+  argTypes: {},
+} as Meta<typeof Button>;
+
+export const Default: StoryObj<typeof Button> = {};
