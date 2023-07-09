e43ee6c7d1434af33171f46ceba084e2a7e3bfac
saku-1101 <sakupi1101@outlook.jp>
2023-07-09 18:03:14 +0900

    update: tsconfigのpathが使えるように

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from '@storybook/nextjs';
+import path from 'path';
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -15,6 +17,16 @@ const config: StorybookConfig = {
       },
     },
   ],
+  webpackFinal: async (config) => {
+    // storybook/nextでts.configのpathを使用する設定
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': path.resolve(__dirname, '../src'),
+      };
+    }
+    return config;
+  },
   framework: {
     name: '@storybook/nextjs',
     options: {},
