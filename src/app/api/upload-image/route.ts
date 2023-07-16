5cffcb01e3ed31fa6b861e1971a89a68ae141e79
saku-1101 <sakupi1101@outlook.jp>
2023-07-16 19:54:19 +0900

    update: markdown editorにD&Dで写真を挿入できる

---
+++ src/app/api/upload-image/route.ts
@@ -0,0 +1,23 @@
+import { Storage } from '@google-cloud/storage';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  console.log(process.env.PROJECT_ID, process.env.BUCKET_NAME, process.env.GOOGLE_APPLICATION_CREDENTIALS);
+  const storage = new Storage({
+    projectId: process.env.PROJECT_ID,
+    keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+  });
+
+  const bucketName = process.env.BUCKET_NAME ?? '';
+  const bucket = storage.bucket(bucketName);
+  const params = req.nextUrl.searchParams;
+
+  const fileObj = bucket.file(params.get('file') as string);
+  const options = {
+    expires: Date.now() + 1 * 60 * 1000,
+    fields: { 'x-goog-meta-test': 'data' },
+  };
+  const [response] = await fileObj.generateSignedPostPolicyV4(options);
+
+  return NextResponse.json(response);
+}
--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,3 +1,3 @@
 import { GraphQLClient } from 'graphql-request';
 
-export const gqlClient = new GraphQLClient(process.env.NEXT_PUBLIC_HASURA_GRAPHQL_URL!, { fetch });
+export const gqlClient = new GraphQLClient(process.env.NEXT_PUBLIC_ENDPOINT_URL!, { fetch });
